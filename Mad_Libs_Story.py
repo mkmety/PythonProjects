d374085a534204b2006e145465218f6408c2ba6d
mkmety <mkmety@gmail.com>
2019-01-22 20:31:16 -0600

    Create Mad_Libs_Story.py

---
+++ Mad_Libs_Story.py
@@ -0,0 +1,11 @@
+first_place = input("Enter the name of a place: ").title()
+first_adj = input("Enter an adjective: ")
+female_celeb = input("Enter the name of a female celebrity: ")
+body_part = input("Enter a body part: ")
+body_part_2 = input("Enter a body part: ")
+human_organ = input("Enter a human organ: ")
+
+print("Once upon a time in a place called " + first_place + ", there was a " + first_adj + " named" + female_celeb) \
+     + "."
+print("Her kingdom was huge, but her " + body_part + " was bigger. She was beautiful from her " + body_part_2 +
+      "to her " + human_organ + ".")
