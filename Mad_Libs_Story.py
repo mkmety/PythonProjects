dbe5f4d873a57f80b6b8c1f307c1dae7c28d88ef
mkmety <mkmety@gmail.com>
2019-01-22 21:05:01 -0600

    Update Mad_Libs_Story.py

--- Mad_Libs_Story.py
+++ Mad_Libs_Story.py
@@ -1,11 +1,31 @@
-first_place = input("Enter the name of a place: ").title()
-first_adj = input("Enter an adjective: ")
+place_1 = input("Enter the name of a place: ")
+adj_1 = input("Enter an adjective: ")
 female_celeb = input("Enter the name of a female celebrity: ")
 body_part = input("Enter a body part: ")
 body_part_2 = input("Enter a body part: ")
 human_organ = input("Enter a human organ: ")
+adj_2 = input("Enter an adjective: ")
+male_celeb = input("Enter the name of a male celebrity: ")
+adj_3 = input("Enter an adjective: ")
+body_part_3 = input("Enter a body part: ")
+body_part_4 = input("Enter a body part: ")
+place_2 = input("Enter the name of a place: ")
+male_celeb_2 = input("Enter the name of a male celebrity: ")
+animal_1 = input("Enter the name of an animal: ")
+verb_1 = input("Enter the name of a verb: ")
+number_1 = input("Enter a number: ")
+body_part_5 = input("Enter a body part: ")
+adj_4 = input("Enter an adjective: ")
+adj_5 = input("Enter an adjective: ")
+past_verb_1 = input("Enter a past tense verb: ")
 
-print("Once upon a time in a place called " + first_place + ", there was a " + first_adj + " named" + female_celeb) \
-     + "."
-print("Her kingdom was huge, but her " + body_part + " was bigger. She was beautiful from her " + body_part_2 +
-      "to her " + human_organ + ".")
+print("\nOnce upon a time in a place called " + place_1 + ", there was a " + adj_1 + " princess named " + female_celeb
+      + ". Her kingdom was huge, but her " + body_part + " was bigger. She was beautiful from her " + body_part_2 +
+      " to her " + human_organ + ". One day she saw a " + adj_2 + " prince named " + male_celeb + ". He had a "
+      + adj_3 + " face. As soon as his " + body_part_3 + " touched her " + body_part_4 + " they fell in love. "
+      "They got married at " + place_2 + " the next day.")
+print("\nNot long after, they had a baby. They decided to call him " + male_celeb_2 + ". He looked like a "
+      + animal_1 + ". He used to " + verb_1 + " " + number_1 + " times a day so that his " + body_part_5
+      + " would be " + adj_4 + " and " + adj_5 + ".")
+print("\nAnd they lived " + past_verb_1 + " ever after!")
+
