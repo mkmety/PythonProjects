815831e170696a65e8ba696315ca1ddf8c9ea4e4
mkmety <mkmety@gmail.com>
2019-01-23 23:30:35 -0600

    Update hangman_game.py
    WIP

--- hangman_game.py
+++ hangman_game.py
@@ -1,45 +1,48 @@
 from random_word import RandomWords
 
+
 r = RandomWords()
-secret_word = r.get_random_word()
-secret_word_len = len(secret_word)
-secret_word_split = list(secret_word)
+secret_word = r.get_random_word().lower()
 counter = 0
+word_guessed = []
+
+for letter in secret_word:
+            word_guessed.append("-")
+
 
 print("Hangman v1.0 - by Matt Kmety")
 print(secret_word)
-print(secret_word_split)
-print("__  " * secret_word_len, end="\n")
 
 while counter < 6:
+    print(word_guessed)
     guess = input("\nGuess a letter: ")
-    print(counter)
-    if guess in secret_word_split:
-        print("Correct!")
-    else:
-        if counter == 0:
-            print("Incorrect!")
-            print(" ------\n |     0\n |\n |\n |\n |\n")
-            counter += 1
-        elif counter == 1:
-            print("Incorrect!")
-            counter += 1
-            print(" ------\n |     0\n |     |\n |     |\n |\n |\n")
-        elif counter == 2:
-            print("Incorrect!")
-            counter += 1
-            print(" ------\n |     0\n |   --|\n |     |\n |\n |\n")
-        elif counter == 3:
-            print("Incorrect!")
-            counter += 1
-            print(" ------\n |     0\n |   --|--\n |     |\n |\n |\n")
-        elif counter == 4:
-            print("Incorrect!")
-            counter += 1
-            print(" ------\n |     0\n |   --|--\n |     |\n |    /  \n |\n")
-        elif counter == 5:
-            print("Incorrect!")
-            counter += 1
-            print(" ------\n |     0\n |   --|--\n |     |\n |    / \\ \n |\n")
-            print("\nYOU LOSE!\nThe word was {}.".format(secret_word))
+    for letter in range(len(secret_word)):
+        if guess == secret_word[letter]:
+            word_guessed[letter] = guess
+        else:
+            if counter == 0:
+                print("Incorrect!")
+                print(" ------\n |     0\n |\n |\n |\n |\n")
+                counter += 1
+            elif counter == 1:
+                print("Incorrect!")
+                counter += 1
+                print(" ------\n |     0\n |     |\n |     |\n |\n |\n")
+            elif counter == 2:
+                print("Incorrect!")
+                counter += 1
+                print(" ------\n |     0\n |   --|\n |     |\n |\n |\n")
+            elif counter == 3:
+                print("Incorrect!")
+                counter += 1
+                print(" ------\n |     0\n |   --|--\n |     |\n |\n |\n")
+            elif counter == 4:
+                print("Incorrect!")
+                counter += 1
+                print(" ------\n |     0\n |   --|--\n |     |\n |    /  \n |\n")
+            elif counter == 5:
+                print("Incorrect!")
+                counter += 1
+                print(" ------\n |     0\n |   --|--\n |     |\n |    / \\ \n |\n")
+                print("\nYOU LOSE!\nThe word was {}.".format(secret_word))
 
