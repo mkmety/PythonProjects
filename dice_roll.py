279ab7846522faba961545985eb75b33c358246d
mkmety <mkmety@gmail.com>
2019-01-24 21:04:32 -0600

    Create dice_roll.py

---
+++ dice_roll.py
@@ -0,0 +1,16 @@
+import random
+from collections import Counter
+
+dice_size = int(input("What sided dice would you like to throw? "))
+times_thrown = int(input("How many times would you like to throw the dice?"))
+count = 0
+results_list = []
+
+while count < times_thrown:
+    result = random.randint(1, dice_size)
+    results_list.append(result)
+    count += 1
+counter_list = Counter(results_list)
+
+for dice in counter_list:
+    print("{:3}  --  {}  --  {}%".format(dice, counter_list[dice], (counter_list[dice] / times_thrown) * 100))
