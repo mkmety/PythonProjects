7826c37ad4cb033bba0a91aeb084e27804acab54
mkmety <mkmety@gmail.com>
2019-01-25 17:37:42 -0600

    Update green_eggs_and_ham.py

--- green_eggs_and_ham.py
+++ green_eggs_and_ham.py
@@ -1,18 +1,6 @@
-import re
-
 file = "green_eggs_and_ham.txt"
 
 with open(file) as f:
     data = f.read()
-    replace = re.sub(r'(i )', r'I ', data)
-#    replace = re.sub(r'(?<= )(i)(?= )', r'I', data)
-#    replace = re.sub(r'(?<=-)(i)(?=-)', r'I', data)
+    replace = data.replace('i ', 'I ').replace('-i-', '-I-')
     print(replace)
-
-    for line in f:
-        #print(line)
-        #data = f.read()
-#        replace = re.sub(r'^i', r'I', line)
-#        replace = re.sub(r'(?<= )(i)(?= )', r'I', line)
-#        replace = re.sub(r'(?<=-)(i)(?=-)', r'I', line)
-        print(replace)
